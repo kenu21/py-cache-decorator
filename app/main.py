dc1ab38c662e064b035806b647b909d74d2e06ae
Yurii <yurij.kenu@gmail.com>
2025-08-30 12:41:56 +0200

    cache: added cache for data

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
+import functools
 from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_storage = {}
+
+    @functools.wraps(func)
+    def wrapper(*args) -> Callable:
+        value = cache_storage.get(args)
+        if value is not None:
+            print("Getting from cache")
+            return value
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            cache_storage[args] = result
+            return result
+
+    return wrapper
