3ca9d20fdb053328add97eb46858dacc8c541008
Yurii <yurij.kenu@gmail.com>
2025-08-30 13:52:21 +0200

    cache: change logic getting from storage and added kwargs

--- app/main.py
+++ app/main.py
@@ -1,20 +1,25 @@
 import functools
-from typing import Callable
+from typing import Callable, Tuple
 
 
 def cache(func: Callable) -> Callable:
     cache_storage = {}
 
+    def make_key(args: Tuple, kwargs: dict) -> Tuple:
+        kwargs_key = tuple(sorted(kwargs.items()))
+        return args + kwargs_key
+
     @functools.wraps(func)
-    def wrapper(*args) -> Callable:
-        value = cache_storage.get(args)
-        if value is not None:
+    def wrapper(*args, **kwargs) -> Callable:
+        key = make_key(args, kwargs)
+
+        if key in cache_storage:
             print("Getting from cache")
-            return value
+            return cache_storage[key]
         else:
             print("Calculating new result")
-            result = func(*args)
-            cache_storage[args] = result
+            result = func(*args, **kwargs)
+            cache_storage[key] = result
             return result
 
     return wrapper
